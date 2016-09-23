66b5ab441db374a255577539759ae2f05957e8be
David Newell <d.newell1@outlook.com>
2016-09-23 19:27:06 +0100

    Add accessors/mutators

--- src/main/java/io/github/daibhin/student_registration_system/CourseProgramme.java
+++ src/main/java/io/github/daibhin/student_registration_system/CourseProgramme.java
@@ -1,11 +1,41 @@
 package io.github.daibhin.student_registration_system;
 
-import java.util.List;
+import java.util.ArrayList;
 import org.joda.time.LocalDate;
 
 public class CourseProgramme {
 	public String name;
-	public List<Module> modules;
+	public ArrayList<Module> modules;
 	public LocalDate startDate;
 	public LocalDate endDate;
+	
+	public CourseProgramme(String name,
+						   ArrayList<Module> modules,
+						   LocalDate startDate,
+						   LocalDate endDate) {
+		this.name = name;
+		this.modules = modules;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ArrayList<Module> getModules() {
+		return modules;
+	}
+	
+	public void addModule(Module module) {
+		modules.add(module);
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
 }
--- src/main/java/io/github/daibhin/student_registration_system/Module.java
+++ src/main/java/io/github/daibhin/student_registration_system/Module.java
@@ -1,9 +1,31 @@
 package io.github.daibhin.student_registration_system;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Module {
 	public String name;
 	public String id;
-	public List<Student> students;
+	public ArrayList<Student> students;
+	
+	public Module(String name, String id, ArrayList<Student> students) {
+		this.name = name;
+		this.id = id;
+		this.students = students;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public ArrayList<Student> getStudents() {
+		return students;
+	}
+	
+	public void addStudent(Student student) {
+		students.add(student);
+	}
 }
