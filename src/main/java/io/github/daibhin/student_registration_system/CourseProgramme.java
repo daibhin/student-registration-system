4643d65b1ce3a5b875e0646b99e324b51f86e86a
David Newell <d.newell1@outlook.com>
2016-09-23 18:49:35 +0100

    Add JodaTime to Course class

--- src/main/java/io/github/daibhin/student_registration_system/CourseProgramme.java
+++ src/main/java/io/github/daibhin/student_registration_system/CourseProgramme.java
@@ -1,8 +1,11 @@
 package io.github.daibhin.student_registration_system;
 
 import java.util.List;
+import org.joda.time.LocalDate;
 
 public class CourseProgramme {
 	public String name;
 	public List<Module> modules;
+	public LocalDate startDate;
+	public LocalDate endDate;
 }
