746cf93cc3737aafe01a84a4075dc3bc57fa3a9e
David Newell <d.newell1@outlook.com>
2016-09-23 18:34:31 +0100

    Setup Maven project

---
+++ src/main/java/io/github/daibhin/student_registration_system/App.java
@@ -0,0 +1,13 @@
+package io.github.daibhin.student_registration_system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
