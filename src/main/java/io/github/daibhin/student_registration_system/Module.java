a5def7234173ff03eea1a8617adb643c7907d356
David Newell <d.newell1@outlook.com>
2016-09-23 18:46:57 +0100

    Add Module class

---
+++ src/main/java/io/github/daibhin/student_registration_system/Module.java
@@ -0,0 +1,9 @@
+package io.github.daibhin.student_registration_system;
+
+import java.util.List;
+
+public class Module {
+	public String name;
+	public String id;
+	public List<Student> students;
+}
